ef3e6cee76171dd50ce975e1e6bef05a242120bf
Kelly McKeown <kpmckeow@uci.edu>
2019-04-17 11:37:31 -0700

    Initial commit.

---
+++ database_scripts/cs122b_ins_1_permission_levels.sql
@@ -0,0 +1,5 @@
+INSERT INTO permission_levels (plevel, pname) VALUES (1, 'ROOT');
+INSERT INTO permission_levels (plevel, pname) VALUES (2, 'ADMIN');
+INSERT INTO permission_levels (plevel, pname) VALUES (3, 'EMPLOYEE');
+INSERT INTO permission_levels (plevel, pname) VALUES (4, 'SERVICE');
+INSERT INTO permission_levels (plevel, pname) VALUES (5, 'USER');
