bf127807afcf6a72358ed164aaa1d6d919495081
omar1vega <omar1vega@gmail.com>
2019-04-17 22:43:38 -0700

    added RegisterRequestModel

---
+++ src/main/java/edu/uci/ics/vegao1/service/idm/models/RegisterRequestModel.java
@@ -0,0 +1,37 @@
+package edu.uci.ics.vegao1.service.idm.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RegisterRequestModel {
+
+    private String email;
+    private char[] password;
+
+    @SuppressWarnings("unused") //Used by Jackson
+    @JsonCreator
+    public RegisterRequestModel(@JsonProperty(value = "email") String email, @JsonProperty(value = "password") char[] password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public String getEmail() {
+        return email;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public char[] getPassword() {
+        return password;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public void setPassword(char[] password) {
+        this.password = password;
+    }
+}
