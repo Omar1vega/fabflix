2c1e5339aa75cf6c5f57ea4102906ca33cd6fe30
omar1vega <omar1vega@gmail.com>
2019-04-21 20:05:48 -0700

    added sessionConfig to ConfigModel

--- src/main/java/edu/uci/ics/vegao1/service/idm/models/ConfigsModel.java
+++ src/main/java/edu/uci/ics/vegao1/service/idm/models/ConfigsModel.java
@@ -6,13 +6,16 @@ public class ConfigsModel {
     private Map<String, String> serviceConfig;
     private Map<String, String> loggerConfig;
     private Map<String, String> databaseConfig;
+    private Map<String, String> sessionConfig;
 
-    public ConfigsModel() { }
+    public ConfigsModel() {
+    }
 
     public Map<String, String> getServiceConfig() {
         return serviceConfig;
     }
 
+    @SuppressWarnings("unused") //Used by Jackson
     public void setServiceConfig(Map<String, String> service) {
         this.serviceConfig = service;
     }
@@ -21,6 +24,7 @@ public class ConfigsModel {
         return loggerConfig;
     }
 
+    @SuppressWarnings("unused") //Used by Jackson
     public void setLoggerConfig(Map<String, String> loggerConfig) {
         this.loggerConfig = loggerConfig;
     }
@@ -29,7 +33,18 @@ public class ConfigsModel {
         return databaseConfig;
     }
 
+    @SuppressWarnings("unused") //Used by Jackson
     public void setDatabaseConfig(Map<String, String> databaseConf) {
         this.databaseConfig = databaseConf;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public Map<String, String> getSessionConfig() {
+        return sessionConfig;
+    }
+
+    @SuppressWarnings("unused") //Used by Jackson
+    public void setSessionConfig(Map<String, String> sessionConfig) {
+        this.sessionConfig = sessionConfig;
     }
 }
