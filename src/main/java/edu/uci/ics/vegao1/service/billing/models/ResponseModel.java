e116671d654f709822e0ca8bd04884fc47ed2ceb
omar1vega <omar1vega@gmail.com>
2019-05-01 22:27:17 -0700

    added credit card ResponseModel enums

--- src/main/java/edu/uci/ics/vegao1/service/billing/models/ResponseModel.java
+++ src/main/java/edu/uci/ics/vegao1/service/billing/models/ResponseModel.java
@@ -12,11 +12,21 @@ public enum ResponseModel {
     QUANTITY_INVALID(33, "Quantity has invalid value."),
     DUPLICATE_INSERTION(311, " Duplicate insertion"),
     ITEM_DOES_NOT_EXIST(312, "Shopping item does not exist"),
+    CREDIT_CARD_INVALID_LENGTH(321, "Credit card ID has invalid length."),
+    CREDIT_CARD_INVALID_VALUE(322, "Credit card ID has invalid value."),
+    CREDIT_CARD_EXPIRATION_INVALID_VALUE(323, "expiration has invalid value."),
+    CREDIT_CARD_DUPLICATE_INSERTION(324, "Duplicate insertion."),
+    CREDIT_CARD_DOES_NOT_EXIST(324, "Credit card does not exist."),
     SHOPPING_CART_INSERT_SUCCESSFUL(3100, "Shopping cart item inserted successfully."),
     SHOPPING_CART_UPDATE_SUCCESSFUL(3100, "Shopping cart item updated successfully."),
     SHOPPING_CART_DELETE_SUCCESSFUL(3120, "Shopping cart item deleted successfully."),
     SHOPPING_CART_RETRIEVED_SUCCESSFUL(3130, "Shopping cart retrieved successfully."),
     SHOPPING_CART_CLEAR_SUCCESSFUL(3140, "Shopping cart cleared successfully."),
+    CREDIT_CARD_INSERT_SUCCESSFUL(3200, "Credit card inserted successfully."),
+    CREDIT_CARD_UPDATE_SUCCESSFUL(3210, "Credit card updated successfully."),
+    CREDIT_CARD_DELETE_SUCCESSFUL(3220, "Credit card deleted successfully."),
+    CREDIT_CARD_RETRIEVE_SUCCESSFUL(3230, "Credit card retrieved successfully."),
+
     VALID_REQUEST(420, "Blaze it");
 
 
