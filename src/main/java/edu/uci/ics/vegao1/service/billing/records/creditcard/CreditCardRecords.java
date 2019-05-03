f7f64f63122fc568e90983e88624998a72abeea2
omar1vega <omar1vega@gmail.com>
2019-05-02 23:15:47 -0700

    updated CreditCardRecords to  use Db utility class

--- src/main/java/edu/uci/ics/vegao1/service/billing/records/creditcard/CreditCardRecords.java
+++ src/main/java/edu/uci/ics/vegao1/service/billing/records/creditcard/CreditCardRecords.java
@@ -1,12 +1,11 @@
 package edu.uci.ics.vegao1.service.billing.records.creditcard;
 
-import edu.uci.ics.vegao1.service.billing.BillingService;
 import edu.uci.ics.vegao1.service.billing.logger.ServiceLogger;
 import edu.uci.ics.vegao1.service.billing.models.ResponseModel;
 import edu.uci.ics.vegao1.service.billing.models.creditcard.CreditCardInsertRequestModel;
+import edu.uci.ics.vegao1.service.billing.util.Db;
 
 import java.sql.Date;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -16,44 +15,23 @@ public class CreditCardRecords {
     private static final String UPDATE_CART_STATEMENT = "UPDATE creditcards SET firstName = ?, lastName = ?, expiration = ? WHERE id= ?";
 
 
-    public static ResponseModel insertCard(CreditCardInsertRequestModel cartInsertRequest) {
+    public static ResponseModel insertCard(CreditCardInsertRequestModel cartInsertRequest) throws ParseException, SQLException {
         ServiceLogger.LOGGER.info("preparing statement to insert creditcard");
+        Date expirationDate = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(cartInsertRequest.getExpiration()).getTime());
+        boolean cardInserted = Db.executeStatement(INSERT_CART_STATEMENT, cartInsertRequest.getFirstName(), cartInsertRequest.getLastName(), expirationDate, cartInsertRequest.getId());
 
-        if (exec(cartInsertRequest, INSERT_CART_STATEMENT)) {
+        if (cardInserted) {
             return ResponseModel.CREDIT_CARD_INSERT_SUCCESSFUL;
         }
         return ResponseModel.CREDIT_CARD_DUPLICATE_INSERTION;
-
     }
 
-    public static ResponseModel updateCardInfo(CreditCardInsertRequestModel creditCardUpdateRequest) {
-        if (exec(creditCardUpdateRequest, UPDATE_CART_STATEMENT)) {
+    public static ResponseModel updateCardInfo(CreditCardInsertRequestModel creditCardUpdateRequest) throws ParseException, SQLException {
+        Date expirationDate = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(creditCardUpdateRequest.getExpiration()).getTime());
+        boolean cardUpdate = Db.executeStatement(UPDATE_CART_STATEMENT, creditCardUpdateRequest.getFirstName(), creditCardUpdateRequest.getLastName(), expirationDate, creditCardUpdateRequest.getId());
+        if (cardUpdate) {
             return ResponseModel.CREDIT_CARD_UPDATE_SUCCESSFUL;
         }
         return ResponseModel.CREDIT_CARD_DOES_NOT_EXIST;
-
-
     }
-
-    private static boolean exec(CreditCardInsertRequestModel model, String query) {
-        try {
-            PreparedStatement statement = BillingService.getCon().prepareStatement(query);
-            statement.setString(4, model.getId());
-            statement.setString(1, model.getFirstName());
-            statement.setString(2, model.getLastName());
-
-            Date expirationDate = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(model.getExpiration()).getTime());
-            statement.setDate(3, expirationDate);
-
-            ServiceLogger.LOGGER.info("Executing query: " + statement.toString());
-            if (statement.executeUpdate() > 0) {
-                return true;
-            }
-        } catch (SQLException e) {
-            ServiceLogger.LOGGER.info("Unable to execute query: " + e.getClass() + e.getCause().getLocalizedMessage());
-        } catch (ParseException ignored) {//already handled
-        }
-        return false;
-    }
-
 }
