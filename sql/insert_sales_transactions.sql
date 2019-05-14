7d89ab089d948992a9979778edbc5917c26c0d2d
vegao1 <vegao1@uci.edu>
2019-05-13 22:12:04 -0700

    updated insert_sales_transactions.sql

--- sql/insert_sales_transactions.sql
+++ sql/insert_sales_transactions.sql
@@ -1,4 +1,9 @@
-CREATE PROCEDURE insert_sales_transactions(IN cartId int, IN paymentToken varchar(50))
+DROP PROCEDURE IF EXISTS insert_sales_transactions;
+
+DELIMITER #
+
+CREATE
+    DEFINER = cs122b_db144@`%.uci.edu` PROCEDURE insert_sales_transactions(IN cartId int, IN paymentToken varchar(50))
 BEGIN
 
     INSERT INTO sales (email, movieId, quantity, saleDate)
@@ -10,4 +15,4 @@ BEGIN
 
     INSERT INTO transactions (sId, token) VALUES (LAST_INSERT_ID(), paymentToken);
 
-END;
+END#
