482dda8da10438a754f4b222b85517b40d09f961
vegao1 <vegao1@uci.edu>
2019-05-19 19:22:02 -0700

    add star get by id

--- src/main/java/edu/uci/ics/vegao1/service/movies/resources/StarsPage.java
+++ src/main/java/edu/uci/ics/vegao1/service/movies/resources/StarsPage.java
@@ -9,16 +9,13 @@ import edu.uci.ics.vegao1.service.movies.records.StarRecords;
 import edu.uci.ics.vegao1.service.movies.records.UserRecords;
 import edu.uci.ics.vegao1.service.movies.validation.UserValidations;
 
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("stars")
+@Path("star")
 public class StarsPage {
     @Path("/search")
     @GET
@@ -44,6 +41,34 @@ public class StarsPage {
         } else {
             return Response.status(Response.Status.BAD_REQUEST).entity(emailCheck).build();
         }
+    }
 
+    @Path("/{id}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getId(@Context HttpHeaders headers, @PathParam("id") String id) {
+        ServiceLogger.LOGGER.info("Received request to retrieve star with id: " + id);
+        String email = headers.getHeaderString("email");
+        String sessionID = headers.getHeaderString("sessionID");
+
+        if (email == null || email.trim().isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(new SearchResponseModel(-16, "Email not provided in request header.")).build();
+        }
+        if (sessionID == null || sessionID.trim().isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(new SearchResponseModel(-17, "SessionID not provided in request header.")).build();
+        }
+
+
+        ResponseModel emailCheck = UserValidations.validateEmail(email);
+        if (emailCheck.equals(ResponseModel.VALID_REQUEST)) {
+            StarRequestModel requestModel = new StarRequestModel();
+            requestModel.setId(id);
+
+            StarResponseModel starResponseModel = StarRecords.searchStars(requestModel, UserRecords.showHidden(email));
+            return Response.status(Response.Status.OK).entity(starResponseModel).build();
+
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).entity(emailCheck).build();
+        }
     }
 }
