71980599a6649c577b6bb182c633c8bdd13eb629
vegao1 <vegao1@uci.edu>
2019-05-22 22:13:32 -0700

    added report retrieval to report page

--- src/main/java/edu/uci/ics/vegao1/service/api_gateway/resources/ReportPage.java
+++ src/main/java/edu/uci/ics/vegao1/service/api_gateway/resources/ReportPage.java
@@ -1,6 +1,9 @@
 package edu.uci.ics.vegao1.service.api_gateway.resources;
 
+import edu.uci.ics.vegao1.service.api_gateway.GatewayService;
 import edu.uci.ics.vegao1.service.api_gateway.logger.ServiceLogger;
+import edu.uci.ics.vegao1.service.api_gateway.records.ResponseModel;
+import edu.uci.ics.vegao1.service.api_gateway.records.ResponseRecords;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,6 +13,7 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
 
 @Path("report")
 public class ReportPage {
@@ -18,28 +22,15 @@ public class ReportPage {
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response report(@Context HttpHeaders headers, String json) {
-        ServiceLogger.LOGGER.info("Received request to report" + json);
-        return Response.status(Response.Status.OK).build();
-
-//        RequestWrapper<CartInsertRequestModel> request = RequestWrapper.map(json, CartInsertRequestModel.class);
-//        if (request.mappedSuccessfully()) {
-//            CartInsertRequestModel cartInsertRequest = request.getRequestModel();
-//
-//            ResponseModel emailCheck = UserValidations.validateEmail(cartInsertRequest.getEmail());
-//            if (emailCheck != ResponseModel.VALID_REQUEST) {
-//                return Response.status(Response.Status.BAD_REQUEST).entity(emailCheck).build();
-//            }
-//
-//            if (cartInsertRequest.getQuantity() < 1) {
-//                return Response.status(Response.Status.OK).entity(ResponseModel.QUANTITY_INVALID).build();
-//            }
-//
-//            return Response.status(Response.Status.OK).entity(CartRecords.insertCart(cartInsertRequest)).build();
-//        } else {
-//            ServiceLogger.LOGGER.info("request mapping was unsuccessful");
-//            return request.getResponse();
-//        }
+    public Response report(@Context HttpHeaders headers, String json) throws SQLException {
+        String transactionID = headers.getHeaderString("transactionID");
+        ServiceLogger.LOGGER.info("Received request to get report for transaction: " + transactionID);
+        ResponseModel report = ResponseRecords.getReport(transactionID);
+        if (report != null) {
+            ServiceLogger.LOGGER.info("Report found: " + report.getJson());
+            return Response.status(report.getStatus()).entity(report.getJson()).build();
+        }
+        ServiceLogger.LOGGER.info("No Response found");
+        return Response.status(Response.Status.NO_CONTENT).header("transactionID", transactionID).header("requestDelay", GatewayService.getGatewayConfigs().getRequestDelay()).build();
     }
-
 }
