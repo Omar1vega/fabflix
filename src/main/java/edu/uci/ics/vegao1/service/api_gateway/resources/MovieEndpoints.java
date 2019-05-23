8298380a1b96b7040581a56164b6f14d8a12282e
vegao1 <vegao1@uci.edu>
2019-05-22 22:12:34 -0700

    added movie endpoints

--- src/main/java/edu/uci/ics/vegao1/service/api_gateway/resources/MovieEndpoints.java
+++ src/main/java/edu/uci/ics/vegao1/service/api_gateway/resources/MovieEndpoints.java
@@ -12,10 +12,7 @@ import javax.ws.rs.core.Response.Status;
 public class MovieEndpoints {
     private String uri = GatewayService.getMovieConfigs().getMoviesUri();
 
-    @Path("search")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response searchMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+    private Response processRequest(Request requestInfo, UriInfo uriInfo, HttpHeaders httpHeaders, String payload) {
         String transactionID = TransactionIDGenerator.generateTransactionID();
         ClientRequest request = new ClientRequest(transactionID, payload, uri, uriInfo.getPathSegments(), requestInfo.getMethod(), httpHeaders.getAcceptableMediaTypes(), httpHeaders.getRequestHeaders(), uriInfo.getQueryParameters());
         GatewayService.getThreadPool().getQueue().enqueue(request);
@@ -23,29 +20,33 @@ public class MovieEndpoints {
         return Response.status(Status.NO_CONTENT).header("transactionID", transactionID).header("requestDelay", GatewayService.getGatewayConfigs().getRequestDelay()).build();
     }
 
+    @Path("search")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response searchMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
+    }
+
     @Path("get/{movieid}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
-        ClientRequest request = new ClientRequest(TransactionIDGenerator.generateTransactionID(), payload, uri, uriInfo.getPathSegments(), requestInfo.getMethod(), httpHeaders.getAcceptableMediaTypes(), httpHeaders.getRequestHeaders(), uriInfo.getQueryParameters());
-        GatewayService.getThreadPool().getQueue().enqueue(request);
-
-        return Response.status(Status.NO_CONTENT).build();
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("add")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addMovieRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response addMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("delete/{movieid}")
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteMovieRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response deleteMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("genre")
@@ -62,52 +63,52 @@ public class MovieEndpoints {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addGenreRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response addGenreRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("genre/{movieid}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getGenresForMovieRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response getGenresForMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("star/search")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response starSearchRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response starSearchRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("star/{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getStarRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response getStarRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("star/add")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addStarRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response addStarRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("star/starsin")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response addStarToMovieRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response addStarToMovieRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 
     @Path("rating")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updateRatingRequest() {
-        return Response.status(Status.NO_CONTENT).build();
+    public Response updateRatingRequest(@Context Request requestInfo, @Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String payload) {
+        return processRequest(requestInfo, uriInfo, httpHeaders, payload);
     }
 }
